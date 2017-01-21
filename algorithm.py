9bf8283152c02e0b14ae4e2df16f2f45253a2f2b
John Bonaguro <jbonaguro2@gmail.com>
2017-01-21 18:02:44 -0500

    Broke algo

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,20 @@
 def algorithm(tickers, prices, averages):
-	print(prices[tickers[0]][-1][1] + .1)
-	return [[tickers[0], "BID", (prices[tickers[0]][-1][1] + .1), 1]]
+	orders = []
+	for t in tickers:
+		if (averages[t][-1][0] > averages[t][-1][1]) and (averages[t][-2][0] < averages[t][-2][1]):
+			flag = False
+			for i in range(5):
+				if (averages[t][(-i-2)][0] > averages[t][(-i-2)][2]):
+					flag = True
+			if not flag:
+				#bid
+				orders.append([t, "BID", (prices[t][-1][1] + .01), 2])
+		elif (averages[t][-1][0] < averages[t][-1][1]) and (averages[t][-2][0] > averages[t][-2][0]):
+			flag = False
+			for i in range(5):
+				if (averages[t][(-i-1)][0] < averages[t][(-i-1)][2]):
+					flag = True
+			if not flag:
+				#sell
+				orders.append([t, "ASK", (prices[t][-1][0] - .01), 2])
+	return orders
