1a8b8173491f010ee46d1a0083ff25e7df27b563
Aaron Parry <aaronparry@Aarons-MacBook-Pro.local>
2017-01-21 14:35:11 -0500

    parse is 'done'

--- parse.py
+++ parse.py
@@ -1,8 +1,20 @@
 import client
 
-TICKERS = ["AMZN", "DIS", "FB", "GOOGL", "IBM", "IVW", "KING", "KO", "NFLX", "TSLA"]
 USERNAME = "rcb"
 PASSWORD = "pass1"
+def getTickers():
+    secList = []
+    sec_str = client.run(USERNAME, PASSWORD, "SECURITIES").split(" ")
+    i = 1
+    while i in range(len(sec_str)):
+        secList.append(sec_str[i])
+        i += 4
+    return secList
+
+TICKERS = getTickers()
+
+
+
 
 def myCash():
     myCashSt = client.run(USERNAME, PASSWORD, "MY_CASH")
@@ -13,10 +25,10 @@ def myCash():
 
 
 def mySecurities():
-    securities = {"AMZN" : [], "DIS" : [], "FB" : [], "GOOGL" : [], "IBM" : [], "IVW" : [], "KING" : [], "KO" : [], "NFLX" : [],"TSLA" : []}
+    securities = {}
     mySecSt = client.run(USERNAME, PASSWORD, "MY_SECURITIES")
     mySec = mySecSt.split(' ')
-    for tick in securities.keys():
+    for tick in TICKERS:
         ind = mySec.index(tick)
         securities[tick] = (int(mySec[ind + 1]), float(mySec[ind + 2]))
     print(securities)
@@ -38,10 +50,29 @@ def myOrders():
     return orders
 
 def securities():
-    print(client.run(USERNAME, PASSWORD, "SECURITIES"))
+    securities = {}
+    secSt = client.run(USERNAME, PASSWORD, "SECURITIES")
+    sec = secSt.split(' ')
+    # print(sec)
+    for tick in TICKERS:
+        ind = sec.index(tick)
+        securities[tick] = (float(sec[ind + 1]), float(sec[ind + 2]), float(sec[ind + 3]))
+    # print(securities)
+    return securities
+
 
 def orders(security):
-    print(client.run(USERNAME, PASSWORD, "ORDERS " + str.upper(security)))
+    myOders = []
+    myOrdersSt = client.run(USERNAME, PASSWORD, "ORDERS " + str.upper(security))
+    myOrders = myOrdersSt.split(' ')[1:]
+    orders = {}
+    for tick in TICKERS:
+        if tick in myOrders:
+            ind = myOrders.index(tick)
+            orders[tick] = (myOrders[ind - 1], float(myOrders[ind + 1]), int(myOrders[ind + 2]))
+
+    # print(orders)
+    return orders
 
 def bid(ticker, price, shares):
     print(client.run(USERNAME, PASSWORD, "BID " + str.upper(ticker) + " " + str(price) + " " + str(shares)))
