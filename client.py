fad4f1f8bc50310db76eb519761301e48d9c830a
Aaron Parry <aaronparry@Aarons-MacBook-Pro.local>
2017-01-21 12:43:06 -0500

    client now returns data

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 import sys
-    
+
 
 def run(user, password, *commands):
-    HOST, PORT = "191.237.6.138", 17429
+    HOST, PORT = "codebb.cloudapp.net", 17429
 
-    
+
     data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -14,13 +14,19 @@ def run(user, password, *commands):
         sock.sendall(bytes(data, "utf-8"))
         sfile = sock.makefile()
         rline = sfile.readline()
+        retString = ""
         while rline:
-            print(rline.strip())
+            retString += rline.strip()
             rline = sfile.readline()
+        # print(retString)
+        return retString
+
+
+
 
 def subscribe(user, password):
     HOST, PORT = "191.237.6.138", 17429
-    
+
     data=user + " " + password + "\nSUBSCRIBE\n"
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
--- parse.py
+++ parse.py
@@ -4,37 +4,37 @@ USERNAME = "rcb"
 PASSWORD = "pass1"
 
 def myCash():
-    client.run(USERNAME, PASSWORD, "MY_CASH")
+    print(client.run(USERNAME, PASSWORD, "MY_CASH"))
 
 def mySecurities():
-    client.run(USERNAME, PASSWORD, "MY_SECURITIES")
+    print(client.run(USERNAME, PASSWORD, "MY_SECURITIES"))
 
 def myOrders():
-    client.run(USERNAME, PASSWORD, "MY_ORDERS")
+    print(client.run(USERNAME, PASSWORD, "MY_ORDERS"))
 
 def securities():
-    client.run(USERNAME, PASSWORD, "SECURITIES")
+    print(client.run(USERNAME, PASSWORD, "SECURITIES"))
 
 def orders(security):
-    client.run(USERNAME, PASSWORD, "ORDERS " + str.upper(security))
+    print(client.run(USERNAME, PASSWORD, "ORDERS " + str.upper(security)))
 
 def bid(ticker, price, shares):
-    client.run(USERNAME, PASSWORD, "BID " + str.upper(ticker) + " " + str(price) + " " + str(shares))
+    print(client.run(USERNAME, PASSWORD, "BID " + str.upper(ticker) + " " + str(price) + " " + str(shares)))
 
 def ask(ticker, price, shares):
-    client.run(USERNAME, PASSWORD, "ASK " + str.upper(ticker) + " " + str(price) + " " + str(shares))
+    print(client.run(USERNAME, PASSWORD, "ASK " + str.upper(ticker) + " " + str(price) + " " + str(shares)))
 
 def clearBid(ticker):
-    client.run(USERNAME, PASSWORD, "CLEAR_BID " + str.upper(ticker))
+    print(client.run(USERNAME, PASSWORD, "CLEAR_BID " + str.upper(ticker)))
 
 def clearAsk(ticker):
-    client.run(USERNAME, PASSWORD, "CLEAR_ASK " + str.upper(ticker))
+    print(client.run(USERNAME, PASSWORD, "CLEAR_ASK " + str.upper(ticker)))
 
 def subscribe():
-    client.run(USERNAME, PASSWORD, "SUBSCRIBE")
+    print(client.run(USERNAME, PASSWORD, "SUBSCRIBE"))
 
 def subscribe():
-    client.run(USERNAME, PASSWORD, "UNSUBSCRIBE")
+    print(client.run(USERNAME, PASSWORD, "UNSUBSCRIBE"))
 
 def close():
-    client.run(USERNAME, PASSWORD, "CLOSE_CONNECTION")
+    print(client.run(USERNAME, PASSWORD, "CLOSE_CONNECTION"))
