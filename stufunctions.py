1f00cb1df369098f6612598ffc80945fb9f1c6d8
Stuart Wallace <stuart@Stuarts-MacBook-Pro.local>
2017-01-21 15:24:56 -0500

    stuff

--- stufunctions.py
+++ stufunctions.py
@@ -1,5 +1,41 @@
 import sys
+import parse
 
 
-def testFunction():
-    print("Test function")
+
+#average price of all asks and bids for ticker
+def bidAskMarket(ticker):
+    print("calculating market value per share for ticker: {}".format(ticker))
+    orders = parse.orders(ticker)
+
+    askshares = 0
+    askpricesum = 0
+    bidshares = 0
+    bidpricesum = 0
+
+    for order in orders:
+        orderType = order[0]
+        orderPrice = order[1]
+        orderNumShares = order[2]
+
+        if orderType == "BID":
+            
+        elif orderType == "ASK":
+            askshares = askshares + numshares
+
+
+
+def checkForOldOrders():
+    print("checking for old orders")
+
+
+
+
+
+
+#TODO market price
+
+
+
+
+getMarketValue("INTL")
