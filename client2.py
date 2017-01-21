f4398b397456318b2a4ba0550194a6e257009a36
Aaron Parry <aaronparry@Aarons-MacBook-Pro.local>
2017-01-21 11:32:52 -0500

    add python client files

---
+++ client2.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+    
+
+def run(user, password, *commands):
+    HOST, PORT = "191.237.6.138", 17429
+    
+    data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+def subscribe(user, password):
+    HOST, PORT = "191.237.6.138", 17429
+    
+    data=user + " " + password + "\nSUBSCRIBE\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
