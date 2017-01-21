36da9dd6632406f8140de90a8cd6af8bf9e73aa9
John Bonaguro <jbonaguro2@gmail.com>
2017-01-21 16:15:19 -0500

    Main loop growing

--- mainloop.py
+++ mainloop.py
@@ -1,20 +1,41 @@
 import parse
 import client
 import time
+import data
 
 def mainloop():
-	running = True
-	while(running):
-		starttime = time.time()
-		#Update data from server
+	try:
+		data.tickers = parse.getTickers()
+		for t in data.tickers:
+			data.my_securities[t] = []
+			data.securities[t] = []
+			#data.orders[t] = []
+			averages = {}
+			prices = {}
+			my_orders = {}
+		running = True
+		while(running):
+			starttime = time.time()
+			#Update data from server
+			ms = parse.mySecurities()
+			for t in data.tickers:
+				data.my_securities[t].append(ms[t])
+			s = parse.securities()
+			for t in data.tickers:
+				data.securities[t].append(s[t])
+			"""for t in data.tickers:
+				o = parse.orders(t)
+				data.orders[t] = o"""
 
-		#Run algorithm
+			#Run algorithm
 
-		#Execute Trades
+			#Execute Trades
 
-		#Wait for 1 sec
-		while((time.time() - starttime) < 1):
-			pass
-		print("Loop")
+			#Wait for 1 sec
+			while((time.time() - starttime) < 1):
+				pass
+			print("Loop")
+	except:
+		pass
 
 mainloop()
