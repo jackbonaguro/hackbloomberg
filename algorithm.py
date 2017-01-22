a5cc1afeb5b37de8e9ce06f34ce3849ea6deb98b
John Bonaguro <jbonaguro2@gmail.com>
2017-01-21 20:21:22 -0500

    stuff

--- algorithm.py
+++ algorithm.py
@@ -8,21 +8,21 @@ def algorithm(tickers, prices, averages, cash):
 		elif (averages[t][-1][0] > averages[t][-1][1]) and (averages[t][-2][0] < averages[t][-2][1]):
 			#Intersections
 			flag = False
-			for i in range(5):
+			for i in range(2):
 				if (averages[t][(-i-2)][0] > averages[t][(-i-2)][2]):
 					flag = True
 			if not flag:
 				#bid
-				orders.append([t, "BID", (prices[t][-1][1] + .01), (cash/(4*(prices[t][-1][1] + .01)))])
+				orders.append([t, "BID", (prices[t][-1][1] + .01), (cash/(4*(prices[t][-1][1] * 1.01)))])
 
-		elif (averages[t][-1][0] < averages[t][-1][1]) and (averages[t][-2][0] > averages[t][-2][0]):
+		elif (averages[t][-1][0] < averages[t][-1][1]) and (averages[t][-2][0] > averages[t][-2][1]):
 			flag = False
-			for i in range(5):
+			for i in range(2):
 				if (averages[t][(-i-1)][0] < averages[t][(-i-1)][2]):
 					flag = True
 			if not flag:
 				#sell
-				orders.append([t, "ASK", (prices[t][-1][0] - .01), (cash/(4*(prices[t][-1][0] - .01)))])
+				orders.append([t, "ASK", (prices[t][-1][0] * .99), (cash/(4*(prices[t][-1][0] * .99)))])
 	return orders
 
 
@@ -33,5 +33,5 @@ def buyUp(tickers, prices, averages, cash):
 		sum += (prices[t][-1][1] + .01)
 	numToBuy = int((cash*.75)/sum)
 	for t in tickers:
-		orders.append([t, "BID", (prices[t][-1][1] + .01), numToBuy])
+		orders.append([t, "BID", (prices[t][-1][1] * 1.01), numToBuy])
 	return orders
